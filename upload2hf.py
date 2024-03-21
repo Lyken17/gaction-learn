7cf8429c08e43f0f9a88524a8d3b4833f6e2af07
Ligeng Zhu <Lyken17@users.noreply.github.com>
2024-03-22 02:23:42 +0800

    Update upload2hf.py

--- upload2hf.py
+++ upload2hf.py
@@ -9,9 +9,6 @@ from huggingface_hub.hf_api import CommitOperationAdd
 from termcolor import colored
 
 
-
-
-
 MAX_UPLOAD_FILES_PER_COMMIT = 64
 MAX_UPLOAD_SIZE_PER_COMMIT = 32 * 1024 * 1024 * 1024  # 64 GiB
 
